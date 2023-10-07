14ee797ec28c1e3ac2a7358fa2471ab84df56237
Ubuntu <ubuntu@ip-172-31-6-238.ap-northeast-3.compute.internal>
2023-10-07 22:07:08 +0000

    dev: for record, seems like it's working

--- src/mavsdk/core/lazy_plugin.h
+++ src/mavsdk/core/lazy_plugin.h
@@ -11,21 +11,32 @@ template<typename Plugin> class LazyPlugin {
 public:
     explicit LazyPlugin(Mavsdk& mavsdk) : _mavsdk(mavsdk) {}
 
-    Plugin* maybe_plugin()
-    {
+    // Plugin* maybe_plugin()
+    // {
+    //     std::lock_guard<std::mutex> lock(_mutex);
+    //     if (_plugin == nullptr) {
+    //         if (_mavsdk.systems().empty()) {
+    //             return nullptr;
+    //         }
+    //         _plugin = std::make_unique<Plugin>(_mavsdk.systems()[0]);
+    //     }
+    //     return _plugin.get();
+    // }
+
+    Plugin* maybe_plugin(int32_t system_id) {
         std::lock_guard<std::mutex> lock(_mutex);
-        if (_plugin == nullptr) {
-            if (_mavsdk.systems().empty()) {
-                return nullptr;
-            }
-            _plugin = std::make_unique<Plugin>(_mavsdk.systems()[0]);
+        auto it = _dict_plugin.find(system_id);
+        if (it == _dict_plugin.end()) {
+            auto system = _mavsdk.systems().at(system_id);
+            _dict_plugin[system_id] = std::make_unique<Plugin>(system);
         }
-        return _plugin.get();
+        return _dict_plugin[system_id].get();
     }
 
 private:
     Mavsdk& _mavsdk;
-    std::unique_ptr<Plugin> _plugin{};
+    // std::unique_ptr<Plugin> _plugin{};
+    std::unordered_map<int, std::unique_ptr<Plugin>> _dict_plugin{};
     std::mutex _mutex{};
 };
 
