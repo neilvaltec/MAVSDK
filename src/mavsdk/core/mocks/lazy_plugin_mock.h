32c0c91808bf532248d15963b6b7a3852ce7025f
Ubuntu <ubuntu@ip-172-31-6-238.ap-northeast-3.compute.internal>
2023-10-08 00:56:31 +0000

    dev: mock method change

--- src/mavsdk/core/mocks/lazy_plugin_mock.h
+++ src/mavsdk/core/mocks/lazy_plugin_mock.h
@@ -6,7 +6,7 @@ namespace testing {
 
 template<typename Plugin> class MockLazyPlugin {
 public:
-    MOCK_CONST_METHOD0(maybe_plugin, Plugin*()){};
+    MOCK_CONST_METHOD1(maybe_plugin, Plugin*(int32_t)){};
 };
 
 } // namespace testing
