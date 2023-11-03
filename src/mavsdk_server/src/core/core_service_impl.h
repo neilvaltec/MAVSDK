e26832438ca8fe3a5f04e5bcc087cbb44a8a92dd
Neil Chou <neil@valtec.io>
2023-11-04 06:07:15 +0800

    dev: AddNewConnection feature - not test with python

--- src/mavsdk_server/src/core/core_service_impl.h
+++ src/mavsdk_server/src/core/core_service_impl.h
@@ -1,5 +1,6 @@
 #include <future>
 #include <string>
+#include <chrono>
 
 #include "core/core.grpc.pb.h"
 #include "mavsdk.h"
@@ -44,6 +45,64 @@ public:
     }
 
     void stop() { _stop_promise.set_value(); }
+
+    grpc::Status AddNewConnection(
+        grpc::ServerContext* /* context */,
+        const rpc::core::AddNewConnectionRequest* request,
+        rpc::core::AddNewConnectionResponse* response) override
+    {
+        bool succeed = false;
+
+        if (request != nullptr) {
+            ConnectionResult connection_result = _mavsdk.add_any_connection(request->connection_url());
+            if (connection_result != ConnectionResult::Success) {
+                std::cerr << "Connection failed: " << connection_result << '\n';
+            }
+        }
+
+        auto system = get_system(_mavsdk);
+
+        if (!system) {
+            succeed = false;
+        }
+
+        if (response != nullptr) {
+            response->set_succeed(succeed);
+        }
+
+        return grpc::Status::OK;
+    }
+
+    std::shared_ptr<System> get_system(Mavsdk& mavsdk)
+    {
+        std::cout << "Waiting to discover system...\n";
+        auto prom = std::promise<std::shared_ptr<System>>{};
+        auto fut = prom.get_future();
+
+        // We wait for new systems to be discovered, once we find one that has an
+        // autopilot, we decide to use it.
+        mavsdk.subscribe_on_new_system([&mavsdk, &prom]() {
+            auto system = mavsdk.systems().back();
+
+            if (system->has_autopilot()) {
+                std::cout << "Discovered autopilot\n";
+
+                // Unsubscribe again as we only want to find one system.
+                mavsdk.subscribe_on_new_system(nullptr);
+                prom.set_value(system);
+            }
+        });
+
+        // We usually receive heartbeats at 1Hz, therefore we should find a
+        // system after around 3 seconds max, surely.
+        if (fut.wait_for(std::chrono::seconds(3)) == std::future_status::timeout) {
+            std::cerr << "No autopilot found.\n";
+            return {};
+        }
+
+        // Get discovered system now.
+        return fut.get();
+    }
 
 private:
     Mavsdk& _mavsdk;
